da49f223e954b26584d7dd8282646fdb8ef6f224
gilarwahibul <gilarwahibul@gmail.com>
2023-11-17 17:08:59 +0700

    push

--- gilarja_test.go
+++ gilarja_test.go
@@ -11,7 +11,7 @@ import (
 //		fmt.Println(data)
 //	}
 func TestGCHandlerFunc(t *testing.T) {
-	data := GCHandlerFunc("MONGOSTRING", "geojson", "post")
+	data := GCHandlerFunc("MONGOSTRING", "petalar", "post")
 
 	fmt.Printf("%+v", data)
 }
